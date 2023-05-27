82b3d562c122f81b39d64ae443e18268573caa02
user name <user email>
2023-05-27 14:52:46 -0700

    Models setup

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    menu_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+                            
